f91a8cca7b82aec6d943d613138be911d5e0e137
harishihan-c <harishihan@outlook.com>
2025-09-20 15:46:47 +0530

    feat: add all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import Background from "./Components/Background/Background";
+import NavBar from "./Components/NavBar/NavBar";
+import Hero from "./Components/Hero/Hero";
+
 function App() {
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  const heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <Background />
+      <Background playStatus={playStatus} heroCount={heroCount} />
       <NavBar />
-      <Hero />
+      <Hero
+        playStatus={playStatus}
+        setPlayStatus={setPlayStatus}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        heroData={heroData[heroCount]}
+      />
     </>
   );
 }
