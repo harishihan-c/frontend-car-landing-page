3971f09cd2bd96c9d3a886999c26690cd59ab83b
harishihan-c <harishihan@outlook.com>
2025-09-20 14:27:25 +0530

    feat: Add Nav Bar structure

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,17 @@
 import "./NavBar.css";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="nav">
+      <div className="logo">Evolve</div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className="contact-card">contact</li>
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
